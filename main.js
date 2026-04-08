ebe341db4cd4e5af7c9f71e6a3709e12ab3e6bf0
MartaWaciega <59935681+MartaWaciega@users.noreply.github.com>
2026-04-08 12:59:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,24 @@
-.chat-window iframe {
-   border: 1px solid darkgrey;
-   border-radius: 0.3rem;
-   box-shadow: 3px 3px lightgrey;
+var chatOpen = false;
+var chatRendered = false;
+// var chatDiv = null;
+// var iframe = null;
+
+function toggleChat() {
+    chatOpen = !chatOpen;
 }
 
-.open-chat {
-   position: fixed;
-   bottom: 1rem;
-   right: 1rem;
-   height: 1.875rem;
-   width: 1.875rem;
-   border-radius: 50%;
-   cursor: pointer;
-}
-
-.chat-window {
-   height: 0;
-   width: 13.75rem;
-   position: fixed;
-   bottom: 4rem;
-   right: 1rem;
-   opacity: 0;
-   overflow: hidden;
-   padding: 0;
-   transition: 
-   height 0.7s ease,
-   opacity 0.6s ease,
-   padding 0.6s ease;
-}
-
-.chat-window.open {
-   height: 16.25rem;
-   opacity: 1;
-   padding: 1rem;
-}
+var btn = document.querySelector('#openChatButton');
+var chatWindow = document.querySelector('.chat-window');
+btn.addEventListener('click', function() {
+    toggleChat();
+    if(!chatRendered) {
+        chatRendered = true;
+    }
+    if (chatOpen) {
+        // chatWindow.className = "chat-window open";
+        chatWindow.classList.add("open");
+    } else {
+        // chatWindow.className = "chat-window";
+        chatWindow.classList.remove("open");
+    }
+});
